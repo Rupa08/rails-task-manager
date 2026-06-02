f07dcc6980ccc82fe90ff2b962d9285c36456ec2
rupa yadav <rupayadav0890@gmail.com>
2026-06-02 15:34:58 +0200

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
+  delete "tasks/:id", to: "tasks#destroy", as: :delete_task
+  get "tasks/:id", to: "tasks#show", as: :task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
 end
